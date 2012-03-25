5c49073167bfc19cb84ce2752f303031301f090a
hiroki.ishiwata@mail.rakuten.com <hiroki.ishiwata@mail.rakuten.com>
2012-03-25 22:32:44 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -13,3 +13,9 @@ exports.getConfig = function(params, callback) {
   var cfg = require("config.js");
   return callback(null, {data: cfg.config});
 };
+
+exports.getConfig = function(params, callback) {
+  console.log('in getConfig with ts:' + Date.now());
+  var cfg = require("config.js");
+  return callback(null, {data: cfg.config});
+};
